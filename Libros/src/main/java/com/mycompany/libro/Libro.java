e88623a74a37122cb604f7e652e28aa01780eb63
Andrei <147158809+Andrei2354@users.noreply.github.com>
2024-10-28 08:08:23 +0000

    Update Libro.java

--- Libros/src/main/java/com/mycompany/libro/Libro.java
+++ Libros/src/main/java/com/mycompany/libro/Libro.java
@@ -1,17 +1,17 @@
 package com.mycompany.libro;
 
 public class Libro {
-    private final int isbn;
+    private final String isbn;
     private final String nombreLibro;
     private boolean utilizando;
 
-    public Libro(int isbn, String nombreLibro) {
+    public Libro(String isbn, String nombreLibro) {
         this.isbn = isbn;
         this.nombreLibro = nombreLibro;
         this.utilizando = false;
     }
 
-    public int obtenerIsbn() {
+    public String obtenerIsbn() {
         return isbn;
     }
 
@@ -26,9 +26,9 @@ public class Libro {
     public synchronized void setPrestado(boolean prestado) {
         this.utilizando = prestado;
         if (prestado) {
-            System.out.println("Libro " + nombreLibro + ", (ISBN: " + isbn + ") se esta prestando.");
+            System.out.println("Libro " + nombreLibro + ", ISBN: " + isbn +" se esta prestando.");
         } else {
-            System.out.println("Libro " + nombreLibro + ", (ISBN: " + isbn + ") se a devuelto.");
+            System.out.println("Libro " + nombreLibro + ", ISBN: " + isbn +" se a devuelto.");
         }
     }
 }
